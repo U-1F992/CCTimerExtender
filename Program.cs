b505f0b41bc6e8f54e54329b0e8034192552e4e7
Kôtaro Mukai <mukai.k1011k@outlook.jp>
2022-03-05 23:53:25 +0900

    benchmark

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using (var app = new CCTimerExtender())
+﻿var sw = new System.Diagnostics.Stopwatch();
+using (var app = new CCTimerExtender())
 {
     // Ctrl+C で強制終了された際にCCTimerを解放する
     Console.CancelKeyPress += ((object? sender, ConsoleCancelEventArgs e) => {app.Dispose();});
@@ -11,9 +12,11 @@
 
 void OnStart(object? sender, EventArgs e)
 {
-    Console.WriteLine("Start: {0}", DateTime.Now.Ticks);
+    sw.Start();
 }
 void OnStop(object? sender, EventArgs e)
 {
-    Console.WriteLine("Stop: {0}", DateTime.Now.Ticks);
+    sw.Stop();
+    Console.WriteLine(sw.ElapsedMilliseconds);
+    sw.Reset();
 }
