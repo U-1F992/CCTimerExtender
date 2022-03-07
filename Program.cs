6c0ee315c55421bf4eed6e346f8bc3b5652dc1cb
Kôtaro Mukai <mukai.k1011k@outlook.jp>
2022-03-07 11:29:05 +0900

    ConsoleAppFramework

--- Program.cs
+++ Program.cs
@@ -1,47 +1,52 @@
 ﻿using System.Diagnostics;
 using System.IO.Ports;
 
-var serialPort = new SerialPort("COM6", 9600);
-serialPort.Open();
-var stopwatch = new Stopwatch();
-
-using (var app = new CCTimerExtender())
+ConsoleApp.Run(args, 
+(
+    [Option("p", "Port name connected.")] string port
+) =>
 {
-    // Ctrl+C で強制終了された際にCCTimerを解放する
-    Console.CancelKeyPress += ((object? sender, ConsoleCancelEventArgs e) => 
+    var serialPort = new SerialPort(port, 9600);
+    serialPort.Open();
+    var stopwatch = new Stopwatch();
+
+    using (var app = new CCTimerExtender())
     {
-        serialPort.Up();
-        serialPort.Close();
-        serialPort.Dispose();
+        // Ctrl+C で強制終了された際にCCTimerを解放する
+        Console.CancelKeyPress += (object? sender, ConsoleCancelEventArgs e) => 
+        {
+            serialPort.Close();
+            serialPort.Dispose();
+            app.Dispose();
+        };
         
-        app.Dispose();
-    });
-    
-    app.OnCountUpTimerStart += OnStart;
-    app.OnCountUpTimerStop += OnStop;
-    
-    do
-    {
-        Console.WriteLine("Enter to press down...");
-        Console.ReadLine();
-        serialPort.Down();
-    } while (!app.HasExited);
-}
+        app.OnCountUpTimerStart += (object? sender, EventArgs e) =>
+        {
+            serialPort.Up();
+            stopwatch.Start();
+        };
+        app.OnCountUpTimerStop += (object? sender, EventArgs e) =>
+        {
+            serialPort.Up();
+            stopwatch.Stop();
+            Console.WriteLine(stopwatch.ElapsedMilliseconds);
+            stopwatch.Reset();
+        };
+        
+        do
+        {
+            Console.WriteLine("Enter to press down...");
+            Console.ReadLine();
+            serialPort.Down();
+        } while (!app.HasExited);
+    }
 
-serialPort.Close();
-serialPort.Dispose();
+    serialPort.Close();
+    serialPort.Dispose();
+});
 
-void OnStart(object? sender, EventArgs e)
+public static class SerialPortExtension
 {
-    serialPort.Up();
-
-    stopwatch.Start();
-}
-void OnStop(object? sender, EventArgs e)
-{
-    serialPort.Up();
-
-    stopwatch.Stop();
-    Console.WriteLine(stopwatch.ElapsedMilliseconds);
-    stopwatch.Reset();
+    public static void Down(this SerialPort serialPort) { serialPort.Write("d"); }
+    public static void Up(this SerialPort serialPort) { serialPort.Write("u"); }
 }
